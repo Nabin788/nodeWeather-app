cece44274b87f22eccb692605269e10c175611ae
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-28 00:08:45 +0545

    node weather app

---
+++ api.js
@@ -0,0 +1,52 @@
+let apiKey = process.env.api_key;
+
+const apiData = async (data,res) => {
+    let {dt, main, weather, sys, wind} = data;
+
+    // extract date and time
+    let date = new Date(dt * 1000);
+    const dateFormated = date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        weekday: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    });
+
+    // extract weather day and icon
+    let weatherDay = weather[0].main;
+    let weatherIcon = weather[0].icon;
+
+    // extract temp . pressure and humidity
+    let {feels_like, pressure, humidity, temp_min, temp_max} = main;
+    let feelsLike = (feels_like - 273.15).toFixed(2);
+    
+    let minTemp = (temp_min - 273.15).toFixed(1);
+    let maxTemp = (temp_max - 273.15).toFixed(1);
+
+    // extract wind
+    let winds = wind.speed;
+
+    // extract country
+    let countryCode = sys.country;
+    let countryName = new Intl.DisplayNames(['es'], {type: 'region'});
+    let country = countryName.of(countryCode);
+
+    const weatherdata = {winds, dateFormated, humidity, pressure, weatherDay, weatherIcon, feelsLike, minTemp, maxTemp, country};
+
+    return weatherdata;
+}
+
+const apiInfo = async (city) => {
+    try {
+        let api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        let data = await api.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { apiInfo, apiData };
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 const path = require("path");
+const bodyParser = require("body-parser");
+const { apiInfo, apiData } = require("./api.js");
 
-const port = 1000;
+const port = process.env.PORT;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // chnaging the default view path because index is within a htm folder
 const viewPath = path.join(__dirname, "./views/html");
@@ -11,7 +17,7 @@ const viewPath = path.join(__dirname, "./views/html");
 // to acces static file like css, js
 // using middleware
 const staticPath = path.join(__dirname, "./views");
-app.use(express.static(staticPath))
+app.use(express.static(staticPath));
 
 // initialize template engine
 app.set("view engine", "hbs");
@@ -21,6 +27,58 @@ app.set("views", viewPath);
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+app.post("/", (req, res) => {
+    const city = req.body.search;
+    apiInfo(city).then(data => {
+        let { name } = data;
+        let myCity = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
+
+        // City found, get weather data
+        apiData(data).then(requireData => {
+            let { winds, dateFormated, humidity, pressure, weatherDay, weatherIcon, feelsLike, minTemp, maxTemp, country } = requireData;
+
+            res.render("index", {
+                cityName: city,
+                country: country,
+                Date: dateFormated,
+                clouds: weatherDay,
+                icons: weatherIcon,
+                minTemp: minTemp,
+                maxTemp: maxTemp,
+                feelsLike: feelsLike,
+                humidity: humidity,
+                pressure: pressure,
+                wind: winds,
+            });
+            if (myCity !== name) {
+                // City not found
+                return res.render("index", {
+                    cityName: "city not found",
+                    country: "",
+                    Date: "",
+                    clouds: "",
+                    icons: "",
+                    minTemp: "",
+                    maxTemp: "",
+                    feelsLike: "",
+                    humidity: "",
+                    pressure: "",
+                    wind: "",
+                });
+            }
+        }).catch(err => {
+            // Handle errors from apiData
+            console.error(err);
+            res.status(500).send("Internal Server Error");
+        });
+    }).catch(err => {
+        // Handle errors from apiInfo
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`server is listening from port number:`, port);
