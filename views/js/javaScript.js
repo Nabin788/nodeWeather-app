3ba1bdf208cd69eb195b47303f0cb54a86c2b259
Nabin Poudel <nabinpoudel788@gmail.com>
2024-08-28 16:31:10 +0545

    notification for invalid city

--- views/js/javaScript.js
+++ views/js/javaScript.js
@@ -1,11 +1,12 @@
 let cityName = document.querySelector(".location-name");
 let weather = document.querySelector(".location-weather");
 
-if(cityName.innerText === ""){
+if(cityName.innerText === "" && !document.body.textContent.includes("Please enter city name.")){
     let div = document.createElement("div"); 
     div.classList.add("notification");
     div.innerText = "Please Enter valid city name."
     document.body.appendChild(div);
-
+    weather.style.visibility = "hidden";
+} else if (document.body.textContent.includes("Please enter city name.")){
     weather.style.visibility = "hidden";
 }
